1439f3a23fc22053abfc659558c11eae86ade4e9
Phil Ashby <phil.github@ashbysoft.com>
2021-11-05 11:31:56 +0000

    review followup: better names for internal variables

--- app/background.cc
+++ app/background.cc
@@ -11,24 +11,24 @@
 
 // Internal state of background processing
 struct background_t {
-    int dbg;
-    bool vid;
+    int debug;
+    bool video;
     volatile bool run;
     cv::VideoCapture cap;
-    int frm;
+    int frame;
     double fps;
     cv::Mat raw;
-    cv::Mat thm;
-    std::thread thr;
-    std::mutex mux;
-    std::mutex mtm;
+    cv::Mat thumb;
+    std::thread thread;
+    std::mutex rawmux;
+    std::mutex thumbmux;
 };
 
 // Internal video reader thread
 static void read_thread(std::shared_ptr<background_t> pbkd) {
-    if (pbkd->dbg) fprintf(stderr, "background: thread start\n");
+    if (pbkd->debug) fprintf(stderr, "background: thread start\n");
     auto last = std::chrono::steady_clock::now();
-    auto proc = last;
+    auto next = last;
     while (pbkd->run) {
         // read new frame - we use a temporary buffer for two reasons:
         // - we can read unlocked, thus we are not impacted by blocking backends (eg: V4L2)
@@ -37,62 +37,62 @@ static void read_thread(std::shared_ptr<background_t> pbkd) {
         if (pbkd->cap.read(grab)) {
             // got a frame, lock and copy for callers
             {
-                std::unique_lock<std::mutex> hold(pbkd->mux);
+                std::unique_lock<std::mutex> hold(pbkd->rawmux);
                 grab.copyTo(pbkd->raw);
-                pbkd->frm += 1;
+                pbkd->frame += 1;
             }
             // grab timing point
             auto now = std::chrono::steady_clock::now();
             // generate thumbnail frame with overlay info if double debug enabled
-            if (pbkd->dbg > 1) {
+            if (pbkd->debug > 1) {
                 char msg[40];
                 long nsec = std::chrono::duration_cast<std::chrono::nanoseconds>(now-last).count();
                 double fps = 1e9/(double)nsec;
                 {
-                    std::unique_lock<std::mutex> hold(pbkd->mtm);
-                    cv::resize(grab, pbkd->thm, cv::Size(160,120));
+                    std::unique_lock<std::mutex> hold(pbkd->thumbmux);
+                    cv::resize(grab, pbkd->thumb, cv::Size(160,120));
                     snprintf(msg, sizeof(msg), "FPS:%0.1f", fps);
-                    cv::putText(pbkd->thm, msg, cv::Point(5,15), cv::FONT_HERSHEY_PLAIN, 1.0, cv::Scalar(0,255,0));
-                    snprintf(msg, sizeof(msg), "FRM:%05d", fps, pbkd->frm);
-                    cv::putText(pbkd->thm, msg, cv::Point(5,30), cv::FONT_HERSHEY_PLAIN, 1.0, cv::Scalar(0,255,0));
-                    cv::putText(pbkd->thm, "Background", cv::Point(5,pbkd->thm.rows-5), cv::FONT_HERSHEY_PLAIN, 1.0, cv::Scalar(0,255,0));
+                    cv::putText(pbkd->thumb, msg, cv::Point(5,15), cv::FONT_HERSHEY_PLAIN, 1.0, cv::Scalar(0,255,0));
+                    snprintf(msg, sizeof(msg), "FRM:%05d", fps, pbkd->frame);
+                    cv::putText(pbkd->thumb, msg, cv::Point(5,30), cv::FONT_HERSHEY_PLAIN, 1.0, cv::Scalar(0,255,0));
+                    cv::putText(pbkd->thumb, "Background", cv::Point(5,pbkd->thumb.rows-5), cv::FONT_HERSHEY_PLAIN, 1.0, cv::Scalar(0,255,0));
                 }
             }
             last = now;
             // wait for next frame, some sources are real-time, others are not, this ensures all play in real-time.
-            proc += std::chrono::nanoseconds((long)(1e9/pbkd->fps));
-            while (now < proc) {
-                std::this_thread::sleep_until(proc);
+            next += std::chrono::nanoseconds((long)(1e9/pbkd->fps));
+            while (now < next) {
+                std::this_thread::sleep_until(next);
                 now = std::chrono::steady_clock::now();
             }
         } else {
             // no more frames, but if we processed some, try to reset position and go again
-            if (pbkd->frm>0 && pbkd->cap.set(cv::CAP_PROP_POS_FRAMES, 0)) {
-                std::unique_lock<std::mutex> hold(pbkd->mux);
-                pbkd->frm = 0;
+            if (pbkd->frame>0 && pbkd->cap.set(cv::CAP_PROP_POS_FRAMES, 0)) {
+                std::unique_lock<std::mutex> hold(pbkd->rawmux);
+                pbkd->frame = 0;
             } else {
                 // unable to reset or previous attempt produced no more frames - stop
-                if (pbkd->dbg) fprintf(stderr, "background: thread stopping at end of stream and not resettable\n");
+                if (pbkd->debug) fprintf(stderr, "background: thread stopping at end of stream and not resettable\n");
                 break;
             }
         }
     }
-    if (pbkd->dbg) fprintf(stderr, "background: thread stop\n");
+    if (pbkd->debug) fprintf(stderr, "background: thread stop\n");
 }
 
 static void drop_background(background_t *pbkd) {
     if (!pbkd)
         return;
-    if (pbkd->vid) {
+    if (pbkd->video) {
         // stop capture
         if (pbkd->run) {
             pbkd->run = false;
-            pbkd->thr.join();
+            pbkd->thread.join();
         }
         // clean up
         pbkd->cap.release();
         pbkd->raw.release();
-        pbkd->thm.release();
+        pbkd->thumb.release();
     } else {
         // clean up
         pbkd->raw.release();
@@ -104,12 +104,12 @@ std::shared_ptr<background_t> load_background(const std::string& path, int debug
     // allocate a shared pointer around storage for the handle, associate custom deleter to clean up when eventually released
     auto pbkd = std::shared_ptr<background_t>(new background_t, drop_background);
     try {
-        pbkd->dbg = debug;
-        pbkd->vid = false;
+        pbkd->debug = debug;
+        pbkd->video = false;
         pbkd->run = false;
         pbkd->cap.open(path, cv::CAP_ANY);    // explicitly ask for auto-detection of backend
         if (!pbkd->cap.isOpened()) {
-            if (pbkd->dbg) fprintf(stderr, "background: cap cannot open: %s\n", path.c_str());
+            if (pbkd->debug) fprintf(stderr, "background: cap cannot open: %s\n", path.c_str());
             return nullptr;
         }
         pbkd->cap.set(cv::CAP_PROP_CONVERT_RGB, true);
@@ -123,30 +123,30 @@ std::shared_ptr<background_t> load_background(const std::string& path, int debug
         if (pbkd->cap.read(pbkd->raw) && pbkd->cap.read(pbkd->raw)) {
             // it's a video, try a reset and start reader thread..
             if (pbkd->cap.set(cv::CAP_PROP_POS_FRAMES, 0))
-                pbkd->frm = 0;
+                pbkd->frame = 0;
             else
-                pbkd->frm = 2;    // unable to reset, so we're 2 frames in
-            pbkd->vid = true;
+                pbkd->frame = 2;    // unable to reset, so we're 2 frames in
+            pbkd->video = true;
             pbkd->run = true;
-            pbkd->thr = std::thread(read_thread, pbkd);
+            pbkd->thread = std::thread(read_thread, pbkd);
         } else {
             // static image file, try loading..
             pbkd->cap.release();
             pbkd->raw = cv::imread(path);
             if (pbkd->raw.empty()) {
-                if (pbkd->dbg) fprintf(stderr, "background: imread cannot open: %s\n", path.c_str());
+                if (pbkd->debug) fprintf(stderr, "background: imread cannot open: %s\n", path.c_str());
                 return nullptr;
             }
         }
-        if (pbkd->dbg)
+        if (pbkd->debug)
             fprintf(stderr, "background properties:\n\tvid: %s\n\tfcc: %08x (%.4s)\n\tfps: %f\n\tcnt: %d\n",
-                pbkd->vid ? "yes":"no", fcc, (char *)&fcc, pbkd->fps, cnt);
+                pbkd->video ? "yes":"no", fcc, (char *)&fcc, pbkd->fps, cnt);
     } catch (std::exception const &e) {
         // oops
-        if (pbkd->dbg) fprintf(stderr, "background: exception while loading: %s\n", e.what());
+        if (pbkd->debug) fprintf(stderr, "background: exception while loading: %s\n", e.what());
         return nullptr;
     } catch (...) {
-        if (pbkd->dbg) fprintf(stderr, "background: unknown exception\n");
+        if (pbkd->debug) fprintf(stderr, "background: unknown exception\n");
         return nullptr;
     }
     return pbkd;
@@ -157,11 +157,11 @@ int grab_background(std::shared_ptr<background_t> pbkd, int width, int height, c
         return -1;
     // static image or video?
     int frm ;
-    if (pbkd->vid) {
+    if (pbkd->video) {
         // grab frame & frame no. under mutex
-        std::unique_lock<std::mutex> hold(pbkd->mux);
+        std::unique_lock<std::mutex> hold(pbkd->rawmux);
         cv::resize(pbkd->raw, out, cv::Size(width, height));
-        frm = pbkd->frm;
+        frm = pbkd->frame;
     } else {
         // resize still image as requested into out
         cv::resize(pbkd->raw, out, cv::Size(width, height));
@@ -173,7 +173,7 @@ int grab_background(std::shared_ptr<background_t> pbkd, int width, int height, c
 int grab_thumbnail(std::shared_ptr<background_t> pbkd, cv::Mat &out) {
     if (!pbkd)
         return -1;
-    std::unique_lock<std::mutex> hold(pbkd->mtm);
-    out = pbkd->thm.clone();
+    std::unique_lock<std::mutex> hold(pbkd->thumbmux);
+    out = pbkd->thumb.clone();
     return 0;
 }
